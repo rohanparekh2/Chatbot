ccdb4cbb05f0833f747c19836420d7d4c98b2fbf
rohanparekh <rohanjp2@illinois.edu>
2023-07-18 10:51:39 -0700

    Added Buttons

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const input = document.getElementById('in');
 const sendButton = document.querySelector('.send_button');
+const initial_message = document.getElementById('initial_message')
 
+  // Initial message
+  const init = document.createElement('div');
+  init.classList.add('init');
+  init.textContent = 'How can we help you today?';
+  initial_message.append(init);
+
+function scroll() {
+  const b = document.querySelector('.body');
+  b.scrollTop = b.scrollHeight;
+}
 function sendMessage() {
   const val = input.value;
 
@@ -19,6 +30,7 @@ function sendMessage() {
   response.textContent = 'We will get back you with an answer as soon as possible.';
   body.append(response)
 
+  scroll();
   // Clears input
   input.value = '';
 }
@@ -31,3 +43,38 @@ input.addEventListener('keydown', (event) => {
     sendMessage();
   }
 });
+
+
+//faq.addEventListener('click', faqClick);
+//social_media.addEventListener('click', social_mediaClick);
+
+function buttonClick(id, link, linktext, message, response) {
+  const button = document.getElementById(id);
+  button.addEventListener('click', () => {
+
+    // Creates a message
+    const m = document.createElement('div');
+    m.classList.add('message');
+    m.textContent = message;
+
+    // Adds message to chat
+    const body = document.querySelector('.body');
+    body.append(m);
+
+    // Creates a response from the bot
+    const r = document.createElement('div');
+    r.classList.add('response');
+    const l = document.createElement('a');
+    l.href = link;
+    l.textContent = linktext;
+    l.target = '_blank';
+    r.textContent = response;
+    r.append(l)
+    body.append(r)
+
+    scroll();
+  });
+}
+
+buttonClick('faq', "https://www.wailofatale.org/faq/", "FAQs", "I have a frequently asked question", 'Here is a link to our frequently asked questions:  ');
+buttonClick('social_media', "https://www.wailofatale.org/#social", "Social Media", "I would like to visit your social media", 'Here is a link to our social media:  ');
